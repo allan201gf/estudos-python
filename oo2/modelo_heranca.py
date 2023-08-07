0731a1381049dfa45958c4e81e496f959f50cc5b
allan201gf <allan201gf@gmail.com>
2023-08-07 20:36:45 -0300

    Herança

---
+++ oo2/modelo_heranca.py
@@ -0,0 +1,41 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1   
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+
+vingadores = Filme("Vingadores guerra infinita", 2018, 160)
+vingadores.dar_likes()
+print(f'{vingadores.nome} - Likes: {vingadores.likes}')
+
+atlanta = Serie("Atlanta", 2017, 2)
+print(atlanta.nome)
