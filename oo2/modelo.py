766a1f199db4be89f174a6ae42eb6e3bf8cbeba8
allan201gf <allan201gf@gmail.com>
2023-08-07 18:55:32 -0300

    POO Python

---
+++ oo2/modelo.py
@@ -0,0 +1,27 @@
+class Filme:
+    def __init__(self, nome, ano, duracao):
+        self.nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
+
+
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
+
+
+vingadores = Filme("Vingadores guerra infinita", 2018, 160)
+print(vingadores.nome)
+
+atlanta = Serie("atlanta", 2017, 2)
+print(atlanta.nome)
