18937d45582b382e30cc146df00f848fdd8ef74f
allan201gf <allan201gf@gmail.com>
2023-08-07 19:08:35 -0300

    Encapsulando comportamento

--- oo2/modelo.py
+++ oo2/modelo.py
@@ -1,9 +1,21 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
     def dar_like(self):
         self.likes += 1
@@ -11,10 +23,22 @@ class Filme:
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.likes = 0
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
     def dar_like(self):
         self.likes += 1
@@ -23,5 +47,5 @@ class Serie:
 vingadores = Filme("Vingadores guerra infinita", 2018, 160)
 print(vingadores.nome)
 
-atlanta = Serie("atlanta", 2017, 2)
+atlanta = Serie("Atlanta", 2017, 2)
 print(atlanta.nome)
