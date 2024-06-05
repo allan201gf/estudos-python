ae321e437332a91111e7622ea26c4fd2b29b4722
allan201gf <allan201gf@gmail.com>
2024-06-04 22:38:19 -0300

    notebook example

--- testes/listasTuplas.py
+++ testes/listasTuplas.py
@@ -6,11 +6,15 @@ print(len(lista))
 # Menor
 print(min(lista))
 
+# Maior
+print(max(lista))
+
 # Valor do index
 print(lista[2])
 
 serie = range(0, 10)
 print(serie[0])
+print(serie)
 
 # Lista que não pode ser alterada
 dias = ("S", "T", "Q", "Q", "S", "S", "D")
